a7b0595d237758e7b08372af050034fed74f5e69
NeoPrint3D <drew@ronsman.com>
2024-09-08 21:03:50 -0500

    env testing

--- packages/web/lib/hono.ts
+++ packages/web/lib/hono.ts
@@ -4,10 +4,8 @@ import type { AppType } from "api";
 export const getApiClient = () => {
   const config = useRuntimeConfig();
   console.log(config);
-  console.log("API_URL", config.public.apiUrl);
-  console.log(process.env.NUXT_PUBLIC_API_HOST_URL);
-
-  return hc<AppType>(
-    config.apiUrl ? `${config.public.apiUrl}/api` : "http://localhost:8080/api"
-  );
+  const apiUrl = process.env.NUXT_ENV_API_HOST_URL;
+  console.log(process.env);
+  console.log("API_URL", apiUrl);
+  return hc<AppType>(apiUrl ? `${apiUrl}/api` : "http://localhost:8080/api");
 };
