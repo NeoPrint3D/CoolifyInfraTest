bcbeaf7a89603eedfd95af2dbbf2b7675f0357cf
NeoPrint3D <drew@ronsman.com>
2024-09-08 19:50:56 -0500

    new stuff

--- packages/web/lib/hono.ts
+++ packages/web/lib/hono.ts
@@ -2,4 +2,8 @@ import { hc } from "hono/client";
 import type { AppType } from "api";
 
 console.log(process.env.API_HOST_URL);
-export const client = hc<AppType>("http://localhost:8080/api");
+export const client = hc<AppType>(
+  process.env.API_HOST_URL
+    ? `${process.env.API_HOST_URL}/api`
+    : "http://localhost:8080/api"
+);
