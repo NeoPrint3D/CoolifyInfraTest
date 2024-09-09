ac285c49850313c0e38dde7c50103e4f4ba87c14
NeoPrint3D <drew@ronsman.com>
2024-09-09 11:30:56 -0500

    get metrics now

--- packages/web/nuxt.config.ts
+++ packages/web/nuxt.config.ts
@@ -3,8 +3,11 @@ import Aura from "@primevue/themes/aura";
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@primevue/nuxt-module"],
-  debug: true,
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@primevue/nuxt-module",
+    "@artmizu/nuxt-prometheus",
+  ],
   primevue: {
     options: {
       theme: {
