332fe45406508cf7db27d90e9e7981d517a11473
NeoPrint3D <drew@ronsman.com>
2024-09-08 20:41:49 -0500

    test

--- packages/web/lib/hono.ts
+++ packages/web/lib/hono.ts
@@ -3,9 +3,10 @@ import type { AppType } from "api";
 
 export const getApiClient = () => {
   const config = useRuntimeConfig();
-  console.log("API_URL", config.apiUrl);
+  console.log(config);
+  console.log("API_URL", config.public.apiUrl);
 
   return hc<AppType>(
-    config.apiUrl ? `${config.apiUrl}/api` : "http://localhost:8080/api"
+    config.apiUrl ? `${config.public.apiUrl}/api` : "http://localhost:8080/api"
   );
 };
