6e12861ad203f69471e4a07d83d419b807cc2ab2
NeoPrint3D <drew@ronsman.com>
2024-09-09 09:15:22 -0500

    maybe a final fix to the env problem

--- packages/web/lib/hono.ts
+++ packages/web/lib/hono.ts
@@ -3,8 +3,9 @@ import type { AppType } from "api";
 
 export const getApiClient = () => {
   const config = useRuntimeConfig();
-  console.log(config);
-  const apiUrl = process.env.NUXT_PUBLIC_API_HOST_URL;
-  console.log(process.env);
-  return hc<AppType>(apiUrl ? `${apiUrl}/api` : "http://localhost:8080/api");
+  return hc<AppType>(
+    config.public.apiUrl
+      ? `${config.public.apiUrl}/api`
+      : "http://localhost:8080/api"
+  );
 };
