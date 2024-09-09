36f7311d265fad71d5962e7b25f9f0a8560f908a
NeoPrint3D <drew@ronsman.com>
2024-09-08 20:34:56 -0500

    debug

--- packages/web/lib/hono.ts
+++ packages/web/lib/hono.ts
@@ -3,6 +3,7 @@ import type { AppType } from "api";
 
 export const getApiClient = () => {
   const config = useRuntimeConfig();
+  console.log("API_URL", config.apiUrl);
 
   return hc<AppType>("https://msggo80kgk4w80w8sgosgkw4.theneocorner.com/api");
 };
