98e0cb36dd5dd77ede5bd7282f9985551d6ccafd
NeoPrint3D <drew@ronsman.com>
2024-09-08 20:19:39 -0500

    new nuxt changes

--- packages/web/lib/hono.ts
+++ packages/web/lib/hono.ts
@@ -1,9 +1,9 @@
 import { hc } from "hono/client";
 import type { AppType } from "api";
 
-const apiUrl = process.env.API_HOST_URL
-  ? `${process.env.API_HOST_URL}/api`
-  : "http://localhost:8080/api";
-console.log(apiUrl);
-
-export const client = hc<AppType>(`${process.env.API_HOST_URL}/api`);
+const config = useRuntimeConfig();
+export const client = hc<AppType>(
+  config.public.apiUrl
+    ? `${config.public.apiUrl}/api`
+    : "http://localhost:8080/api"
+);
--- packages/web/nuxt.config.ts
+++ packages/web/nuxt.config.ts
@@ -12,4 +12,9 @@ export default defineNuxtConfig({
       },
     },
   },
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.API_HOST_URL,
+    },
+  },
 });
